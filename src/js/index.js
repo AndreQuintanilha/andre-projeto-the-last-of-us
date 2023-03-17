1ba65903555b0f93fbf5dbf304bfe0f9753e3804
AndreQuintanilha <127791266+AndreQuintanilha@users.noreply.github.com>
2023-03-17 13:08:24 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-/* 
+/*
 	OBJETIVO - quando clicarmos no botão temos que mostrar a imagem de fundo correspondente
 
     - passo 1 - dar um jeito de pegar o elemento HTML dos botões
@@ -14,37 +14,41 @@
 	- passo 6 - fazer aparecer a imagem de fundo correspondente ao botão clicado
 */
 
-//console.log() - mostrar mensagem e açoes na aba do console no inspecionar ou clicando F12
-//Exemplo
-//console.log(BATATA) 
-//console.log(document)
-
-
-
-//passo 1 - dar um jeito de pegar o elemento HTML dos botões
+// passo 1 - dar um jeito de pegar o elemento HTML dos botões
 const botoesCarrossel = document.querySelectorAll('.botao');
-// console.log(document.querySelectorAll('.botao')); -lista os 6 botoes la no console como teste.
 const imagens = document.querySelectorAll('.imagem');
-//console.log(document.querySelectorAll('.imagem')); - lista as imagens no console.
 
 // passo 2 - dar um jeito de identificar o clique do usuário no botão
-// forEach - para cada 
 botoesCarrossel.forEach((botao, indice) => {
     botao.addEventListener('click', () => {
-       
         // passo 3 - desmarcar o botão selecionado anterior      
-        const botaoSelecionado = document.querySelector('.selecionado');
-        botaoSelecionado.classList.remove('selecionado');
+        desativarBotaoSelecionado();
+
         // passo 4 - marcar o botão clicado como se estivesse selecionado
-        botao.classList.add('selecionado');
+        selecionarBotaoCarrosel(botao);
 
         // passo 5 - esconder a imagem ativa de fundo
-        const imagemAtiva = document.querySelector('.ativa');
-        imagemAtiva.classList.remove('ativa');
+        esconderImagemAtiva();
 
         // passo 6 - fazer aparecer a imagem de fundo correspondente ao botão clicado
-        // indice - é a posição de um elemento dentro de uma lista de elementos
-        imagens[indice].classList.add('ativa');
+        mostrarImagemDeFundo(indice);
     })
 })
 
+function mostrarImagemDeFundo(indice) {
+    imagens[indice].classList.add('ativa');
+}
+
+function selecionarBotaoCarrosel(botao) {
+    botao.classList.add('selecionado');
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa');
+    imagemAtiva.classList.remove('ativa');
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+}
